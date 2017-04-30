7668dc9a57882682fbbe23919a7c5effa826dbb9
Jenna Provazek <jennaprovazek@yahoo.com>
2017-04-30 13:49:00 -0700

    added date to dataview

--- backend/server/api/images.js
+++ backend/server/api/images.js
@@ -18,27 +18,48 @@ exports.register = function (server, options, next) {
                 }, {
                     name: 'Wood Chair',
                     location: '324 N. Chorro Street',
-                    url: '/images/chair.jpg'
+                    url: '/images/chair.jpg',
+                    time: '12/15/1988'
                 }, {
                     name: 'Men\'s Shirt',
                     location: '123 Higuera Street',
-                    url: '/images/shirt.jpg'
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
                 }, {
                     name: 'Size 10 Dress Shoes',
                     location: '1230 Monte Vista Place',
-                    url: '/images/sheos.jpg'
+                    url: '/images/sheos.jpg',
+                    time: '12/15/1988'
                 }, {
                     name: 'Desk',
                     location: '3385 Ivan Way',
-                    url: '/images/desk.jpeg'
+                    url: '/images/desk.jpeg',
+                    time: '12/15/1988'
                 }, {
                     name: 'Men\'s Shirt',
                     location: '888 Apple Way',
-                    url: '/images/shirt.jpg'
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
                 }, {
                     name: 'Men\'s Shirt',
                     location: '111 Crazy Lane',
-                    url: '/images/shirt.jpg'
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
+                }, {
+                    name: 'Men\'s Shirt',
+                    location: '111 Crazy Lane',
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
+                }, {
+                    name: 'Men\'s Shirt',
+                    location: '111 Crazy Lane',
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
+                }, {
+                    name: 'Men\'s Shirt',
+                    location: '111 Crazy Lane',
+                    url: '/images/shirt.jpg',
+                    time: '12/15/1988'
                 }]
             });
         }
