395d2ac8d9568377766d6a249a47da9b62521c63
jeffxie888 <jeffreyxie888@gmail.com>
2017-05-06 15:19:38 -0700

    login UI changes

--- frontend/app/view/login/Login.js
+++ frontend/app/view/login/Login.js
@@ -9,35 +9,81 @@ Ext.define('Fc.view.login.Login', {
 
     controller: 'login',
     bodyPadding: 10,
-    title: 'Login Window',
+    //title: 'SLO FreeCycle',
+    title: '<div style="text-align:center;">SLO FreeCycle</div>',
     closable: false,
     autoShow: true,
+    width: '25%',
 
-    items: {
-        xtype: 'form',
-        reference: 'form',
-        items: [{
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
-            allowBlank: false
+    items: [{
+            xtype: 'form',
+            reference: 'form',
+            items: [{
+                xtype: 'textfield',
+                name: 'username',
+                emptyText: 'Username',
+                width: '100%',
+                //fieldLabel: 'Username',
+                allowBlank: false
+            }, {
+                xtype: 'textfield',
+                blankText: 'password',
+                name: 'password',
+                emptyText: 'Password',
+                width: '100%',
+                inputType: 'password',
+                //fieldLabel: 'Password',
+                allowBlank: false
+            }],
+            buttonAlign: 'center',
+            buttons: [{
+                text: 'Login',
+                formBind: true,
+                listeners: {
+                    click: 'onLoginClick'
+                }
+            }]
         }, {
-            xtype: 'textfield',
-            name: 'password',
-            inputType: 'password',
-            fieldLabel: 'Password',
-            allowBlank: false
+            xtype : 'menuseparator',
+            width : '100%'
         }, {
-            xtype: 'displayfield',
-            hideEmptyLabel: false,
-            value: 'Enter any non-blank password'
-        }],
-        buttons: [{
-            text: 'Login',
-            formBind: true,
-            listeners: {
-                click: 'onLoginClick'
-            }
+            xtype: 'form',
+            reference: 'form',
+            buttonAlign: 'center',
+            items: [{
+                xtype: 'textfield',
+                name: 'firstname',
+                margin: '8px 0px',
+                width: '100%',
+                emptyText: 'First Name',
+                allowBlank: false
+            },{
+                xtype: 'textfield',
+                name: 'email',
+                width: '100%',
+                emptyText: 'Cal Poly Email',
+                allowBlank: false
+            }, {
+                xtype: 'textfield',
+                name: 'password',
+                width: '100%',
+                inputType: 'password',
+                emptyText: 'Password',
+                allowBlank: false
+            }, {
+                xtype: 'textfield',
+                name: 'repeatpassword',
+                width: '100%',
+                inputType: 'password',
+                emptyText: 'Repeat Password',
+                allowBlank: false
+            }],
+            buttons: [{
+                text: 'Sign up!',
+                formBind: true,
+                listeners: {
+                    click: 'onLoginClick'
+                }
+            }]
         }]
-    }
 });
