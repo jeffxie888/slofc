e55d7512161adfe8a2bb5517783a80aab9f41a8d
jeffxie888 <jeffreyxie888@gmail.com>
2017-05-13 14:25:57 -0700

    changed register.js

--- frontend/app/view/login/Register.js
+++ frontend/app/view/login/Register.js
@@ -15,48 +15,17 @@ Ext.define('Fc.view.login.Register', {
     autoShow: true,
     width: '25%',
 
-    items: [/*{
+    items: [{
             xtype: 'form',
             reference: 'form',
+            buttonAlign: 'center',
             items: [{
                 xtype: 'textfield',
-                name: 'username',
-                emptyText: 'Username',
+                name: 'fullname',
                 width: '100%',
-                //fieldLabel: 'Username',
+                emptyText: 'Full Name',
                 allowBlank: false
             }, {
-                xtype: 'textfield',
-                blankText: 'password',
-                name: 'password',
-                emptyText: 'Password',
-                width: '100%',
-                inputType: 'password',
-                //fieldLabel: 'Password',
-                allowBlank: false
-            }],
-            buttonAlign: 'center',
-            buttons: [{
-                text: 'Sign Up',
-                listeners: {
-                    //click: 'onRegisterClick'
-                }
-            }]
-        }, {
-            xtype : 'menuseparator',
-            width : '100%'
-        },*/ {
-            xtype: 'form',
-            reference: 'form',
-            buttonAlign: 'center',
-            items: [{
-                xtype: 'textfield',
-                name: 'firstname',
-                margin: '8px 0px',
-                width: '100%',
-                emptyText: 'First Name',
-                allowBlank: false
-            },{
                 xtype: 'textfield',
                 name: 'email',
                 width: '100%',
@@ -64,17 +33,17 @@ Ext.define('Fc.view.login.Register', {
                 allowBlank: false
             }, {
                 xtype: 'textfield',
+                name: 'username',
+                margin: '8px 0px',
+                width: '100%',
+                emptyText: 'Username',
+                allowBlank: false
+            }, {
+                xtype: 'textfield',
                 name: 'password',
                 width: '100%',
                 inputType: 'password',
                 emptyText: 'Password',
-                allowBlank: false
-            }, {
-                xtype: 'textfield',
-                name: 'repeatpassword',
-                width: '100%',
-                inputType: 'password',
-                emptyText: 'Repeat Password',
                 allowBlank: false
             }],
             buttons: [{
